45259be918b96e8d4f14fd01c83c887f9c5ed4ba
Luca <lightlucc@gmail.com>
2026-01-02 11:31:43 +0200

    fix case sensivity

--- src/main/java/com/lucatm11/IpLimiter/Utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.lucatm11.iplimiter.utils;
-
-import org.bukkit.ChatColor;
-
-public class Utils {
-    public static String colorize(String text) {
-        return ChatColor.translateAlternateColorCodes('&', text);
-    }
-}
