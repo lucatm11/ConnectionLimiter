b9887e073bc5d0227723ea2c685e6126f5cbfd63
Luca <lightlucc@gmail.com>
2026-01-08 16:57:21 +0200

    Implementation of the notifications system

--- src/main/java/com/lucatm11/connectionlimiter/listeners/Join.java
+++ src/main/java/com/lucatm11/connectionlimiter/listeners/Join.java
@@ -1,5 +1,6 @@
 package com.lucatm11.connectionlimiter.listeners;
 
+import com.lucatm11.connectionlimiter.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,24 +19,38 @@ public class Join implements Listener {
 
     @EventHandler
     public void onLogin(PlayerLoginEvent event) {
-        Player player = event.getPlayer();
         String ip = event.getAddress().getHostName();
-
         plugin.connection.initializeConnection(ip);
 
-        if (plugin.connection.getConnections(ip) >= plugin.settings.maxConnectionsAllowedPerIp) {
-            if(!player.hasPermission("connectionlimiter.bypass")) {
-                if(plugin.settings.punishmentEnabled) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), plugin.settings.punishmentCommand.replace("{player}", player.getName()));
-                    if(plugin.settings.punishmentDisallowEntry) {
-                        event.disallow(Result.KICK_OTHER, plugin.messages.tooManyConnections);
-                    }
-                } else {
-                    event.disallow(Result.KICK_OTHER, plugin.messages.tooManyConnections);
+        if(plugin.connection.getConnections(ip) < plugin.settings.maxConnectionsAllowedPerIp) {
+            plugin.connection.addConnection(ip);
+            return;
+        }
+
+        if(event.getPlayer().hasPermission("connectionlimiter.exempt")) {
+            plugin.connection.addConnection(ip);
+            return;
+        }
+
+        if (plugin.settings.punishmentEnabled) {
+            String cmd = plugin.settings.punishmentCommand.replace("{player}", event.getPlayer().getName());
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
+
+            if (!plugin.settings.punishmentDisallowEntry) return;
+        }
+
+        event.disallow(Result.KICK_OTHER, plugin.messages.tooManyConnections);
+
+        if(plugin.settings.notificationsEnabled) {
+            String message = plugin.messages.notifyMessage.replace("{player}", event.getPlayer().getName());
+
+            Bukkit.getConsoleSender().sendMessage(message);
+
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if(player.hasPermission("connectionlimiter.notify")) {
+                    player.sendMessage(message);
                 }
             }
-        } else {
-            plugin.connection.addConnection(ip);
         }
     }
 }
--- src/main/java/com/lucatm11/connectionlimiter/utils/configurations/Configurations.java
+++ src/main/java/com/lucatm11/connectionlimiter/utils/configurations/Configurations.java
@@ -14,7 +14,7 @@ public class Configurations {
         public final String serverReloadKick;
         public final String incorrectUsage;
         public final String incorrectCommand;
-
+        public final String notifyMessage;
         public Messages(YamlConfiguration config) {
             noPermission = Utils.colorize(config.getString("errors.no-permission"));
             tooManyConnections = Utils.colorize(config.getString("too-many-connections-kick"));
@@ -25,7 +25,7 @@ public class Configurations {
             serverReloadKick = Utils.colorize(config.getString("reload.server-reload-kick"));
             incorrectUsage = Utils.colorize(config.getString("errors.incorrect-usage"));
             incorrectCommand = Utils.colorize(config.getString("errors.incorrect-command"));
-        }
+            notifyMessage = Utils.colorize(config.getString("notifications.notification-message"));}
     }
 
     public class Settings {
@@ -34,13 +34,17 @@ public class Configurations {
         public final Boolean punishmentEnabled;
         public final String punishmentCommand;
         public final Boolean punishmentDisallowEntry;
+        public final Boolean notificationsEnabled;
+        public final Boolean notifyConsole;
 
         public Settings(YamlConfiguration config) {
             maxConnectionsAllowedPerIp = config.getInt("general.max-connections-allowed-per-ip");
             kickAllPlayersOnServerReload = config.getBoolean("general.kick-all-players-on-server-reload");
-            punishmentEnabled = config.getBoolean("general.punishment.enable");
-            punishmentCommand = config.getString("general.punishment.command");
-            punishmentDisallowEntry = config.getBoolean("general.punishment.disallow-entry");
+            punishmentEnabled = config.getBoolean("punishment.enable");
+            punishmentCommand = config.getString("punishment.command");
+            punishmentDisallowEntry = config.getBoolean("punishment.disallow-entry");
+            notificationsEnabled = config.getBoolean("notifications.enable");
+            notifyConsole = config.getBoolean("notifications.console");
         }
     }
 }
